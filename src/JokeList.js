da54f7c180c27b8991d5a46eec14a246e44bb72e
Richard Dowdy <richard.dowdy0@gmail.com>
2020-03-06 15:07:36 -0800

    everything loads properly on the first render WIP re-render on update of votes

--- src/JokeList.js
+++ src/JokeList.js
@@ -2,73 +2,160 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Joke from "./Joke";
 import "./JokeList.css";
+import { render } from "@testing-library/react";
 
-function JokeList({ numJokesToGet = 10 }) {
-  const [jokes, setJokes] = useState([]);
+//{ numJokesToGet = 10 }
+class JokeList extends React.Component {
+  static defaultProps = { numJokesToGet: 10 };
+  
+  constructor(props) {
+    super(props);
+    this.state = { jokes: [], seenJokes: [] }
+  }
+
+  // const [jokes, setJokes] = useState([]);
 
   /* get jokes if there are no jokes */
 
-  useEffect(function() {
-    async function getJokes() {
-      let j = [...jokes];
-      let seenJokes = new Set();
-      try {
-        while (j.length < numJokesToGet) {
-          let res = await axios.get("https://icanhazdadjoke.com", {
-            headers: { Accept: "application/json" }
-          });
-          let { status, ...jokeObj } = res.data;
-  
-          if (!seenJokes.has(jokeObj.id)) {
-            seenJokes.add(jokeObj.id);
-            j.push({ ...jokeObj, votes: 0 });
-          } else {
-            console.error("duplicate found!");
-          }
-        }
-        setJokes(j);
-      } catch (e) {
-        console.log(e);
-      }
+  async componentDidMount() {
+    try {
+      console.log(this.props.numJokesToGet)
+      const apiJokes = await this._getJokes(this.props.numJokesToGet);
+      const jokes = apiJokes.map(joke => { return { ...joke, votes: 0 } });
+      this.setState({ jokes });
+    } catch (err) {
+      console.log(err);
     }
+    
+    /// YOUR STUFF ~~~~
+    
+    // let j = [...this.state.jokes];
+    // let seenJokes = new Set();
+    // console.log();
+    
+    // try {
+    //   while (this.state.jokes.length < this.props.numJokesToGet) {
+    //     let res = await axios.get("https://icanhazdadjoke.com", {
+    //       headers: { Accept: "application/json" }
+    //     });
+    //     let { status, ...jokeObj } = res.data;
+        
+    //     if (!seenJokes.has(jokeObj.id)) {
+    //       // console.log(seenJokes)
+    //       seenJokes.add(jokeObj.id);
+    //       // j.push({ ...jokeObj, votes: 0 });
+    //     } else {
+    //       console.error("duplicate found!");
+    //     }
+    //   }
+    //   // this.setState({ jokes: j })
+    // } catch (e) {
+    //   console.log(e);
+    // }
+  }
 
-    if (jokes.length === 0) getJokes();
-  }, [jokes, numJokesToGet]);
+  async _getJokes(numJokes) {
+    const headers = { Accept: "application/json" };
+    const path = `https://icanhazdadjoke.com/search?limit=${numJokes}`;
+    const { data } = await axios.get(path, { headers });
+    return data.results;
+  }
+
+  // async componentDidUpdate() {
+  //   let j = [...this.state.jokes];
+  //   let seenJokes = new Set();
+  //   try {
+  //     while (this.state.jokes.length < this.props.numJokesToGet) {
+  //       let res = await axios.get("https://icanhazdadjoke.com", {
+  //         headers: { Accept: "application/json" }
+  //       });
+  //       let { status, ...jokeObj } = res.data;
+
+  //       if (!seenJokes.has(jokeObj.id)) {
+  //         seenJokes.add(jokeObj.id);
+  //         j.push({ ...jokeObj, votes: 0 });
+  //       } else {
+  //         console.error("duplicate found!");
+  //       }
+  //     }
+  //     this.setState({ jokes: j })
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // }
+
+  // useEffect(function() {
+  //   async function getJokes() {
+  //     let j = [...jokes];
+  //     let seenJokes = new Set();
+  //     try {
+  //       while (j.length < numJokesToGet) {
+  //         let res = await axios.get("https://icanhazdadjoke.com", {
+  //           headers: { Accept: "application/json" }
+  //         });
+  //         let { status, ...jokeObj } = res.data;
+
+  //         if (!seenJokes.has(jokeObj.id)) {
+  //           seenJokes.add(jokeObj.id);
+  //           j.push({ ...jokeObj, votes: 0 });
+  //         } else {
+  //           console.error("duplicate found!");
+  //         }
+  //       }
+  //       setJokes(j);
+  //     } catch (e) {
+  //       console.log(e);
+  //     }
+  //   }
+
+  //   if (jokes.length === 0) getJokes();
+  // }, [jokes, numJokesToGet]);
 
   /* empty joke list and then call getJokes */
 
-  function generateNewJokes() {
-    setJokes([]);
-  }
+  // generateNewJokes() {
+  //   this.componentDidUpdate()
+  // }
+  // function generateNewJokes() {
+  //   setJokes([]);
+  // }
 
   /* change vote for this id by delta (+1 or -1) */
 
-  function vote(id, delta) {
-    setJokes(allJokes =>
-      allJokes.map(j => (j.id === id ? { ...j, votes: j.votes + delta } : j))
-    );
+  vote(id, delta) {
+    this.setState({
+      jokes: allJokes => allJokes.map(j =>
+        j.id === id ? { ...j, votes: j.votes + delta } : j)
+    })
   }
+  // function vote(id, delta) {
+  // setJokes(allJokes =>
+  //   allJokes.map(j => (j.id === id ? { ...j, votes: j.votes + delta } : j))
+  // );
 
   /* render: either loading spinner or list of sorted jokes. */
+  
+  render() {
+  if (this.state.jokes.length) {
+    let sortedJokes = [...this.state.jokes].sort((a, b) => b.votes - a.votes);
+      return (
+        <div className="JokeList">
+          <button className="JokeList-getmore" onClick={this.generateNewJokes}>
+            Get New Jokes
+          </button>
 
-  if (jokes.length) {
-    let sortedJokes = [...jokes].sort((a, b) => b.votes - a.votes);
-  
-    return (
-      <div className="JokeList">
-        <button className="JokeList-getmore" onClick={generateNewJokes}>
-          Get New Jokes
-        </button>
-  
-        {sortedJokes.map(j => (
-          <Joke text={j.joke} key={j.id} id={j.id} votes={j.votes} vote={vote} />
-        ))}
-      </div>
-    );
+          {sortedJokes.map(j => (
+            <Joke text={j.joke} key={j.id} id={j.id} votes={j.votes} vote={this.vote} />
+          ))}
+        </div>
+      );
+
+    }
+    return null;
   }
+};
 
-  return null;
 
-}
+
 
 export default JokeList;
